750a8a1a5f54a58e1b006fc2ddcd95ec9d4c5197
정승길/Onestore/SKP <sg.jueng@sk.com>
2016-06-30 10:58:36 +0900

    문법 및 템플릿 사용법 예시 코드

--- simple-rest/src/main/resources/static/js/controllers.js
+++ simple-rest/src/main/resources/static/js/controllers.js
@@ -4,4 +4,69 @@ angular.module('app.controllers', [])
 	$scope.roomState = {};
 	$scope.roomState['13-1'] = "사용중";
 })
+
+.controller('LoginPageCtrl', ['$window', '$rootScope', '$scope', '$state', 'LoginService', function($window, $rootScope, $scope, $state, LoginService) {
+	
+	$scope.login = function (user) {
+		console.log(user);
+		LoginService.login(user)
+		.success(function (response) {
+			$state.go('menu.dashboard');
+			$window.location.reload(true);
+		});
+	};
+	
+}])
+
+.controller('SideMenuCtrl', ['$window', '$rootScope', '$scope', '$state', 'ChartService', 'LoginService', function($window, $rootScope, $scope, $state, ChartService, LoginService) {
+	$rootScope.loginState = false;
+	
+	$scope.getCategoryList = function () {
+		ChartService.getCategoryList()
+		.success(function(data, status) {
+			console.log("response : ", data, status);
+			$scope.categoryList = data;
+			$rootScope.loginState = true;
+		});
+	};
+	
+	$scope.logout = function() {
+		LoginService.logout()
+		.success(function() {
+			$rootScope.loginState = false;
+			$rootScope.username = null;
+			$scope.categoryList = null;
+			
+			$state.go('menu.login');
+			//$window.location.reload(true);
+		});
+	};
+	
+	$scope.init =  function() {
+		return LoginService.getUserInfo()
+			   .success(function(user) {
+				  console.log("getUserInfo response : ", user);
+				  
+				  if (user && user != "") {
+					  $rootScope.loginState = true;
+					  $rootScope.username = user;
+					  $scope.getCategoryList();
+				  } else {
+					  $state.go('menu.login');
+				  }
+				  return;
+			   });
+		;
+	};
+	
+//	$scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+//		console.log("on $stateChangeSuccess : SideMenuCtrl ", toState, toParams);
+//		
+//		if (toState.name == "menu.login" && $rootScope.loginState == true) {
+//			$state.go('menu.dashboard');
+//		}
+//	});
+	
+	$scope.init();
+}]);
  
--- simple-rest/src/main/resources/static/js/services.js
+++ simple-rest/src/main/resources/static/js/services.js
@@ -1,10 +1,64 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
-
+.factory('ChartService', ['$http', '$q', function($http, $q){
+	
+	return {
+		
+		getCategoryList : function() {
+			console.log('call getCategoryList');
+			return $http.get('/icms-app-statistics-web/chart/');
+		},
+		
+		getChartList : function(categoryId) {
+			console.log('call getChartList : ', categoryId);
+			return $http.get('/icms-app-statistics-web/chart/' + categoryId);
+		},
+		
+		getChartData : function(categoryId, chartId, userParams) {
+			console.log('call getChartData : ' , categoryId, " / ", chartId, "userParams : ", userParams);
+			return $http.get('/icms-app-statistics-web/chart/' + categoryId + '/' + chartId, { params : userParams });
+		}
+	};
 }])
 
-.service('BlankService', [function(){
-
+.factory('AnalysisService', ['$http', '$q', 'CommonService', function($http, $q, commonService){
+		
+	return {
+		
+		getRankAppReport : function(chartUrl, start, number) {
+			return $http.get('/icms-app-statistics-web/analysis/googleRankApp?chartUrl=' + chartUrl + "&start=" + start + "&number=" + number);
+		},
+		
+		downloadRankAppReport : function(categoryList, chartUrl, number) {
+			
+			var chartUrlList = "";
+			categoryListParam = categoryList.forEach( 
+				function (item) {
+					chartUrlList += "&chartUrlList=" + item.url + chartUrl;
+				}
+			); 
+			console.log('/icms-app-statistics-web/analysis/googleRankApp/excel?' + "number=" + number + chartUrlList);
+			
+			commonService.saveFile('/icms-app-statistics-web/analysis/googleRankApp/excel?' + "number=" + number + chartUrlList);
+			//return $http.get('/icms-app-statis\tics-web/analysis/googleRankApp/excel?' + "number=" + number + chartUrlList);
+		},
+		
+		updateRankAppReport : function(chartUrl) {
+			return $http.post('/icms-app-statistics-web/analysis/googleRankApp', "chartUrl=" + chartUrl, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}});
+		},
+		
+		getSingleRankAppReport : function(packageName) {
+			return $http.get('/icms-app-statistics-web/analysis/googleSingleApp?packageName=' + packageName);
+		},
+		
+		linkApp : function(cid, packageName) {
+			return $http.post('/icms-app-statistics-web/analysis/linkApp/' + cid, { packageName : packageName } );
+		},
+		
+		unLinkApp : function(cid) {
+			return $http.delete('/icms-app-statistics-web/analysis/linkApp/' + cid);
+		}
+		
+	};
 }]);
 
